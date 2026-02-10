7396acce2a47fbb295a8a2b19db8e26d8a0b99c4
sameerakhtar11 <sameerakhtar1011@gmail.com>
2026-02-10 14:33:03 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,8 +14,8 @@ export const signUp=async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure: process.env.NODE_ENVIRONMENT === "production",
-            sameSite: process.env.NODE_ENVIRONMENT === "production" ? "none" : "strict",
+            secure: true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -40,8 +40,8 @@ export const login = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure: process.env.NODE_ENVIRONMENT === "production",
-            sameSite: process.env.NODE_ENVIRONMENT === "production" ? "none" : "strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
